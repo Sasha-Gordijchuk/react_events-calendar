816f224a8e70c53f068f2cd3a63c4f65e4d01ea6
Sasha Gordijchuk <san4ez1717@gmail.com>
2023-01-28 18:20:52 +0200

    eslint was setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 export const App: React.FC = () => {
   return (
@@ -6,4 +7,4 @@ export const App: React.FC = () => {
       123
     </div>
   );
-}
+};
--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 export const Calendar: React.FC = () => {
   return (
     <div>Calendar</div>
-  )
-}
-
+  );
+};
